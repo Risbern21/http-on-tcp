41493c597b2089c050c693ca9d6f3f9cf2abe999
risbern21 <risbernfernandes21@gmail.com>
2025-08-28 16:34:03 +0530

    binary encoding

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,12 +10,21 @@ import (
 	"strings"
 	"syscall"
 
+	"http.go/internal/headers"
 	"http.go/internal/request"
 	"http.go/internal/response"
 	"http.go/internal/server"
 )
 
 const port = 42069
+
+func toStr(bytes []byte) string {
+	out := ""
+	for _, b := range bytes {
+		out += fmt.Sprintf("%02x", b)
+	}
+	return out
+}
 
 func respond400() []byte {
 	return []byte(`<html>
@@ -65,7 +75,25 @@ func main() {
 		} else if r.RequestLine.RequestTarget == "/myproblem" {
 			body = respond500()
 			status = response.StatusInternalServerError
-		} else if strings.HasPrefix(r.RequestLine.RequestTarget, "/httpbin/stream") {
+		} else if strings.HasPrefix(r.RequestLine.RequestTarget, "/video") {
+			f, err := os.ReadFile("assets/vim.mp4")
+			if err != nil {
+				w.WriteStatusLine(response.StatusInternalServerError)
+				w.WriteHeaders(h)
+				body = respond500()
+				w.WriteBody(body)
+				return
+			}
+
+			h.Replace("Content-Type", "video/mp4")
+			h.Replace("Content-Length", fmt.Sprintf("%d", len(f)))
+
+			w.WriteStatusLine(response.StatusOK)
+			w.WriteHeaders(h)
+			w.WriteBody(f)
+			return 
+
+		} else if strings.HasPrefix(r.RequestLine.RequestTarget, "/httpbin/") {
 			target := r.RequestLine.RequestTarget
 			r, err := http.Get("https://httpbin.org/" + target[len("/httpbin/"):])
 			if err != nil {
@@ -77,23 +105,35 @@ func main() {
 				h.Delete("content-length")
 				h.Set("content-encoding", "chunked")
 				h.Replace("Content-Type", "text/plain")
-				
+
+				h.Set("Trailer", "X-Content-SHA256")
+				h.Set("Trailer", "X-Content-Length")
 				w.WriteHeaders(h)
 
+				fullBody := []byte{}
 				for {
 					data := make([]byte, 32)
 					n, err := r.Body.Read(data)
-					if err!=nil{
+					if err != nil {
 						break
 					}
+
+					fullBody = append(fullBody, data[:n]...)
 					//write the hex
-					w.WriteBody([]byte(fmt.Sprintf("%x\r\n",n)))
+					w.WriteBody([]byte(fmt.Sprintf("%x\r\n", n)))
 					//write the data
 					w.WriteBody(data[:n])
 					w.WriteBody([]byte("\r\n"))
 				}
-				w.WriteBody([]byte("0\r\n\r\n"))
-				return 
+				w.WriteBody([]byte("0\r\n"))
+				trailers := headers.NewHeaders()
+				out := sha256.Sum256(fullBody)
+				trailers.Set("X-Content-SHA256", toStr(out[:]))
+				trailers.Set("X-Content-Length", fmt.Sprintf("%d", len(fullBody)))
+				w.WriteHeaders(trailers)
+				w.WriteBody([]byte("\r\n"))
+
+				return
 			}
 		}
 
